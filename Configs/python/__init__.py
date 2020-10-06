419d10eda8d55ec6e9ac845bd389eeeba9041ac1
AdrianoDee <adriano.diflorio@ba.infn.it>
2020-10-06 10:41:31 +0200

    Cleaning unused folder

---
+++ Configs/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/CMS_HLT_Phase2_Tracking/Configs/',1)[0])+'/cfipython/slc7_amd64_gcc820/CMS_HLT_Phase2_Tracking/Configs')
