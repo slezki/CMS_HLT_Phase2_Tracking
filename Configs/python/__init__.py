630d7952a408bc78331d1b92aa43aecaafbdbf18
AdrianoDee <AdrianoDee@users.noreply.github.com>
2020-10-06 15:30:45 +0200

    Delete __init__.py

--- Configs/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/CMS_HLT_Phase2_Tracking/Configs/',1)[0])+'/cfipython/slc7_amd64_gcc820/CMS_HLT_Phase2_Tracking/Configs')
