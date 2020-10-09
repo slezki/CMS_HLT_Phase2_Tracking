073cc58e7889e93518fe9c716c9a9fefd2e58a43
AdrianoDee <AdrianoDee@users.noreply.github.com>
2020-10-09 07:41:59 +0200

    Track Pt Max to 100 for sorter

--- wfs/tracking_modules.py
+++ wfs/tracking_modules.py
@@ -21,7 +21,7 @@ hltPhase2SeedFromProtoTracks = cms.PSet(
 
 hltPhase2PSetPvClusterComparerForIT = cms.PSet(
   track_chi2_max = cms.double( 20.0 ),
-  track_pt_max = cms.double( 80.0 ),
+  track_pt_max = cms.double( 100.0 ),
   track_prob_min = cms.double( -1.0 ),
   track_pt_min = cms.double( 1.0 )
 )
